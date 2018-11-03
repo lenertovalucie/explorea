ec6b6ba82fe56b41af7490c01751c8898f367985
Lucie Lenertova <lenertova.lucie@gmail.com>
2018-11-03 19:45:18 +0100

    Added Event.__str__() method in events/models.py

--- events/models.py
+++ events/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 
 # Create your models here.
-from django.db import models
 
 class Event(models.Model):
 
     name = models.CharField(max_length=200)
     description = models.TextField(max_length=1000)
     location = models.CharField(max_length=500)
-    
     category = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
