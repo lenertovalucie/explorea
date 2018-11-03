40c734fb1942233c93c598f00a4aacd2364a8e9e
Lucie Lenertova <lenertova.lucie@gmail.com>
2018-11-03 20:03:27 +0100

    Created new model EventRun in events/models.py

--- events/models.py
+++ events/models.py
@@ -11,3 +11,12 @@ class Event(models.Model):
 
     def __str__(self):
         return self.name
+
+class EventRun(models.Model):
+
+    event = models.ForeignKey(Event, 
+        on_delete=models.CASCADE)
+    happens = models.DateTimeField(blank=False, null=False)
+    seats_available = models.PositiveIntegerField(blank=False, null=False)
+    price = models.DecimalField(max_digits=10, 
+        decimal_places=2, blank=False, null=False)
