4c231d0905aeb252c8a5f9a4f91d20a263aa1849
Lucie Lenertova <lenertova.lucie@gmail.com>
2018-11-08 20:05:50 +0100

    Added profile view into accounts.views. Added accounts.urls.py. Added templates.accounts.profile.html.

--- config/urls.py
+++ config/urls.py
@@ -18,5 +18,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('', include('explorea.events.urls')),
+    path('accounts/', include('explorea.accounts.urls')),
     path('admin/', admin.site.urls),
 ]
