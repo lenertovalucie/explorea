d63dbb01fc5347c702c63bdcb80af93f4587b005
Lucie Lenertova <lenertova.lucie@gmail.com>
2018-11-10 15:12:28 +0100

    Added Django login view and accounts/login.html

--- explorea/accounts/urls.py
+++ explorea/accounts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 from . import views
 
 urlpatterns = [
+    path('login/', auth_views.LoginView.as_view(template_name="accounts/login.html"), name='login'),
     path('profile/', views.profile, name='profile'),
 ]
