8c5dfb38899beccf5a42b9966fe600e505a68cf2
Lucie Lenertova <lenertova.lucie@gmail.com>
2018-11-10 15:44:10 +0100

    Added logout and accounts/logged_out.html

--- explorea/accounts/urls.py
+++ explorea/accounts/urls.py
@@ -4,6 +4,8 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(template_name="accounts/login.html"), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    #path('logout/', auth_views.LogoutView.as_view(template_name='accounts/logged_out.html'), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='/events/'), name='logout'),
     path('profile/', views.profile, name='profile'),
 ]
