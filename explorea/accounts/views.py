f8caf799777dd96190376581c82354ca78886483
Lucie Lenertova <lenertova.lucie@gmail.com>
2018-11-10 15:53:05 +0100

    Added conditional LOG IN vs LOG OUT rendering into base.html. Removed unnecessary context dict arguemnt to the render() in accounts/views.py profile()

--- explorea/accounts/views.py
+++ explorea/accounts/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 def profile(request):
     
-    return render(request, 'accounts/profile.html', {'user': request.user})
+    return render(request, 'accounts/profile.html')
