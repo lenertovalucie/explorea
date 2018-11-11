6a3c7bb6e714234d33cab36c6be848bd3ad8a0c4
Lucie Lenertova <lenertova.lucie@gmail.com>
2018-11-11 13:39:29 +0100

    homework - event detail view, template

--- explorea/events/views.py
+++ explorea/events/views.py
@@ -1,26 +1,18 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from .models import Event
-# Create your views here.
+from .models import Event, EventRun
 
 
 def index(request):
     return render(request, 'events/index.html')
 
 def event_listing(request):
-
     events = Event.objects.all()
 
     return render(request, 'events/event_listing.html', {'events': events})
 
 def event_detail(request, name):
-    data = {'Chill' : '<h2>Chill on the beach just for $400</h2>',
-            'Camping': '<h2>Camp with us for $50</h2>',
-            'Flying': '<h2>Fly for free</h2>'}
+    event = Event.objects.get(name=name)
+    runs = EventRun.objects.filter(event=event)
 
-    selection = data.get(name)
-
-    if selection:
-        return HttpResponse(selection)
-    else:
-        return HttpResponse('No such event in offering for now')
+    return render(request, 'events/event_detail.html', {'runs': runs, 'event':event})
