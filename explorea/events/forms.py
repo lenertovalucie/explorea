c851635f75807fbed00993e1c7391362d3632e7a
Lucie Lenertova <lenertova.lucie@gmail.com>
2018-11-24 19:28:07 +0100

    validate date, category

--- explorea/events/forms.py
+++ explorea/events/forms.py
@@ -14,11 +14,9 @@ class EventForm(forms.ModelForm):
         ]
 
 class EventRunForm(forms.ModelForm):
+    date = forms.DateField(input_formats=["%d.%m.%Y"], 
+        widget=forms.DateInput(format = '%d.%m.%Y'))
 
     class Meta:
         model = EventRun
-        fields = [
-
-            'seats_available',
-            'price'
-        ]
+        exclude = ['event']
--- explorea/events/models.py
+++ explorea/events/models.py
@@ -4,16 +4,32 @@ from datetime import datetime as dt
 
 
 class Event(models.Model):
+    FUN = 'FN'
+    RELAX = 'RX'
+    EXP = 'EX'
+    SIGHTS = 'SI'
+
+    CATEGORY_CHOICES= (
+        (FUN, 'fun'),
+        (RELAX, 'relax'),
+        (EXP, 'experience'),
+        (SIGHTS, 'sights')
+    )
 
     name = models.CharField(max_length=200)
     description = models.TextField(max_length=1000)
     location = models.CharField(max_length=500)
-    category = models.CharField(max_length=20)
+    category = models.CharField(
+        max_length=20,
+        choices = CATEGORY_CHOICES,
+        default = FUN,
+    )
     host = models.ForeignKey(User, 
         on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
+        
 
 class EventRun(models.Model):
 
